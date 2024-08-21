b8b8ab0aef621091235d9a46eb6dc59e9c11f13c
Amira Khaled <155392660+amirakhaled2027@users.noreply.github.com>
2024-08-21 23:14:49 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+// Write three functions to calculate the bitwise AND, bitwise OR and bitwise XOR of two numbers.
+
+
+//bitwise AND &
+//bitwise OR |
+//bitwise XOR ^
+
+function bitwiseAND(a, b){
+    return a & b;
+}
+console.log(bitwiseAND(7,12));
+
+
+function bitwiseOR(a, b){
+    return a | b;
+}
+console.log(bitwiseOR(7, 12));
+
+
+function bitwiseXOR(a, b){
+    return a ^ b;
+}
+console.log((bitwiseXOR(7, 12)));
